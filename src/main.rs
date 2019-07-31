0406b425a9745b7bd7591f2d5065452d9b8f90d8
nargok <nargok@gmail.com>
2019-08-01 08:42:53 +0900

    四角形を求める：構造なし

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,14 @@
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
-}
-
 fn main() {
-  let mut user1 = build_user(String::from("someone@example.com"), String::from("sum"));
+    let width1 = 30;
+    let height1 = 50;
 
-  user1.email = String::from("anotheremail@example.com");
-
-  let user2 = User {
-      email: String::from("user2@example.com"),
-      username: String::from("user2@example.com"),
-      ..user1
-  };
-
-  println!("User1's email is: {}", user1.email);
-  println!("User2;s email is: {}, active_status is: {}", user2.email, user2.active);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
 }
 
-fn build_user(email: String, username: String) -> User {
-  User {
-      email,
-      username,
-      active: true,
-      sign_in_count: 1,
-  }
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
 
